d65d1c0cece8d44ee4dd6483a2a78f16c6c32713
Polina Kyzmina <paina@MacBook-Air-Polina.local>
2022-12-09 02:41:44 +0700

    cltkfyj

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// Домашняя работа, задание 2.
+/*
+
+Console.Write("Input a number1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a number2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+{
+    Console.WriteLine(num1 + "наибольшее значение." + num2 + "наименьшее значение");
+}
+else 
+{
+    Console.WriteLine(num2 + "наибольшее значение." + num1 + "наименьшее значение");
+}
+*/
+
+// задание 4
+/*
+Console.Write("Input a number1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a number2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a number3");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (max<num2)
+{
+    max = num2;
+}
+else
+{
+ if (max<num3)
+ {
+    Console.WriteLine(num3);
+ }
+    else
+    {
+        Console.WriteLine(max);
+    }
+ }
+if (max<num3)
+{
+    Console.WriteLine(num3);   
+}
+else
+{
+     Console.WriteLine(max); 
+}
+*/
+
+// задача 6
+/*
+Console.Write("Input a number1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 1)
+{
+Console.WriteLine("нечетное");
+}
+else
+{
+   Console.WriteLine("четное"); 
+}
+*/
+ 
+ // задача 8
+/*
+Console.Write("Input a number1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while (current <= num1)
+{
+    Console.Write(current + " ");
+current = current + 2;
+}
+*/
+j
+
+
+
